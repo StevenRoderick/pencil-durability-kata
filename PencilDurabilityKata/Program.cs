d8fc9535b1bd0ea45020adffa1d7e389011086a5
Steven Roderick <dukeroderick@gmail.com>
2019-05-20 22:41:01 -0500

    Created Projects

---
+++ PencilDurabilityKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pencil_durability_kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
